34b05996b00a9a7201bbf84841012275684db6bf
EmmaStuderus <105861357+EmmaStuderus@users.noreply.github.com>
2022-06-02 20:53:16 +1000

    Add files via upload

--- LibManager/LibManager/Membermenu.cs
+++ LibManager/LibManager/Membermenu.cs
@@ -39,7 +39,7 @@ namespace LibManager
                     second = movie.NoBorrowings;
                     top3[1].Title = movie.Title;
                 }
-                else 
+                else
                 {
                     third = movie.NoBorrowings;
                     top3[2].Title = movie.Title;
@@ -48,105 +48,87 @@ namespace LibManager
             return top3;
         }
 
-        //Emma: I don't think we need this
-        /*
-        public static void MemberDetails()
-        {
-            Console.Write("Enter the first name");
-            string Fname = Console.ReadLine();
-            Console.WriteLine("Enter the last name");
-            string Lname = Console.ReadLine();
-            Console.WriteLine("Enter the password");
-            string password = Console.ReadLine();
-            int pword = Int32.Parse(password);
-            //return movietitle;
-        }
-        */
 
         public static void Init(IMemberCollection thisMembersCollection, IMovieCollection thisMovieCollection, IMember member)
         {
-            
-            string choice;
-
             bool status = true;
 
-            while (status == true)
+            while (status)
             {
                 PrintMemberMenu();
-                status = false;
+                switch (Console.ReadLine())
+                {
+                    // Displaying the information about all the movies in alphabetical order and number of available copies
+                    case "1":
 
-            }
-            switch (choice = Console.ReadLine())
-            {
-                // Displaying the information about all the movies in alphabetical order and number of available copies
-                case "1": 
-
-                    IMovie[] allMovies = thisMovieCollection.ToArray();
-                    // If there is any movie in the library print the title and available copies
-                    if (allMovies.Length != 0)
-                    {
-                        foreach (IMovie movie in allMovies)
+                        IMovie[] allMovies = thisMovieCollection.ToArray();
+                        // If there is any movie in the library print the title and available copies
+                        if (allMovies.Length != 0)
                         {
-                            Console.Write(movie.Title + " available copies: ");
-                            Console.WriteLine(movie.AvailableCopies);
+                            foreach (IMovie movie in allMovies)
+                            {
+                                Console.Write(movie.Title + " available copies: ");
+                                Console.WriteLine(movie.AvailableCopies);
+                            }
                         }
-                    }
-                    else
-                    {
-                        Console.WriteLine("There is no movie in the library!");
-                    }
- 
+                        else
+                        {
+                            Console.WriteLine("There is no movie in the library!");
+                        }
 
-                    break;
-                // Displaying the information of a movie when provided with title
-                case "2": 
-                    Console.Write("Title of the movie : ");
-                    string movietitle = Console.ReadLine();
-                    if (thisMovieCollection.Search(movietitle) != null)
-                    {
-                        movietitle.ToString();
 
-                    }
-                    else
-                        Console.WriteLine("Error the movie does not exist!");
-                    break;
+                        break;
+                    // Displaying the information of a movie when provided with title
+                    case "2":
+                        Console.Write("Title of the movie : ");
+                        string movietitle = Console.ReadLine();
+                        if (thisMovieCollection.Search(movietitle) != null)
+                        {
+                            movietitle.ToString();
 
-                // Borrow a movie DVD from the library
-                case "3":
-                    // Prompt the user for a tile and read it
-                    Console.Write("Title of the movie : ");
-                    string movietitle2 = Console.ReadLine();
-                    thisMovieCollection.Search(movietitle2).AddBorrower(member);
-                    break;
+                        }
+                        else
+                            Console.WriteLine("Error the movie does not exist!");
+                        break;
 
-                case "4":
-                    // Prompt the user for a tile and read it
-                    Console.Write("Title of the movie : ");
-                    string movietitle3 = Console.ReadLine();
-                    // Remove the member from the borrower list of that particular movie
-                    thisMovieCollection.Search(movietitle3).RemoveBorrower(member);
-                    // Increase the available copies of that particular movie
-                    thisMovieCollection.Search(movietitle3).AvailableCopies++;
-                    break;
+                    // Borrow a movie DVD from the library
+                    case "3":
+                        // Prompt the user for a tile and read it
+                        Console.Write("Title of the movie : ");
+                        string movietitle2 = Console.ReadLine();
+                        thisMovieCollection.Search(movietitle2).AddBorrower(member);
+                        break;
 
-                case "5":
-                    Console.WriteLine("List of movies borrowed by " + member.ToString());
-                    //member.MoviesBorrowed
-                    break;
+                    case "4":
+                        // Prompt the user for a tile and read it
+                        Console.Write("Title of the movie : ");
+                        string movietitle3 = Console.ReadLine();
+                        // Remove the member from the borrower list of that particular movie
+                        thisMovieCollection.Search(movietitle3).RemoveBorrower(member);
+                        // Increase the available copies of that particular movie
+                        thisMovieCollection.Search(movietitle3).AvailableCopies++;
+                        break;
 
-                case "6":
-                    Console.WriteLine("Top three most borrowed movies are: " );
-                    IMovie[] topMovies = FindTopThree(thisMovieCollection.ToArray());
-                    foreach (IMovie movie in topMovies)
-                    {
-                        Console.WriteLine(movie.Title + " " + movie.NoBorrowings);
-                    }
-                    
-                    break;
+                    case "5":
+                        Console.WriteLine("List of movies borrowed by " + member.ToString());
+                        //member.MoviesBorrowed
+                        break;
 
-                case "0":
-                    Mainmenu.PrintMainMenu();
-                    break;
+                    case "6":
+                        Console.WriteLine("Top three most borrowed movies are: ");
+                        IMovie[] topMovies = FindTopThree(thisMovieCollection.ToArray());
+                        foreach (IMovie movie in topMovies)
+                        {
+                            Console.WriteLine(movie.Title + " " + movie.NoBorrowings);
+                        }
+
+                        break;
+
+                    case "0":
+                        status = false;
+                        Mainmenu.PrintMainMenu();
+                        break;
+                }
             }
         }
     }
